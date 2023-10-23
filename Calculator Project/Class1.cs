5743a79dd0c28ceaef7618130294f14463cb11e7
somnaidu <somnaidu@LIN-JXK28G3>
2023-10-23 16:55:30 +0530

    Add project files.

---
+++ Calculator Project/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Project
+{
+    internal class Class1
+    {
+        public delegate int MyDelegate1(int a, int b);
+
+        
+
+        
+
+            public int Add(int a, int b)
+
+            {
+
+                return a + b;
+
+            }
+
+            public int Substract(int a, int b)
+
+            {
+
+                return a - b;
+
+            }
+
+            public int Multiply(int a, int b)
+
+            {
+
+                return a * b;
+
+            }
+
+            public int Divide(int a, int b)
+
+            {
+
+                return a / b;
+
+            }
+
+
+
+        }
+    }
+
